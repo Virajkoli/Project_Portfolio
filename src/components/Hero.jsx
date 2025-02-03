ebd3c35caade265704477f2945d5e93f7ff88046
Viraj Koli <koliviraj555@gmail.com>
2025-02-04 00:33:19 +0530

    Hero section updates and 3D Model added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section className="relative h-screen w-full mx-auto">
+      <div
+        className={`${styles.paddingX} inset-0 top-[120px] absolute mx-auto flex flex-row items-start gap-5`}
+      >
+        <div className="flex flex-col items-center justify-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#915eff]"></div>
+          <div className="w-1 sm:h-80 h-40 violet-gradient"></div>
+        </div>
+        <div>
+          <h1 className={`${styles.heroHeadText}`}>
+            Hii i am <span className="text-[#915eff]">Viraj</span>
+          </h1>
+          <p className={`${styles.heroSubText}`}>I am a MERN stack developer</p>
+        </div>
+      </div>
+      <ComputersCanvas />
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
